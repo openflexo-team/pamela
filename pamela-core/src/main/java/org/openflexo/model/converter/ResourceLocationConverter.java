c748e0ae2247b5a1af5ef84910ad07a09bdaf072
Fabien Dagnat <fabien.dagnat@telecom-bretagne.eu>
2016-06-07 10:23:18 +0200

    MEDIUM / Lots of formatting, adding genericity parameters,
    import unused, var unused, some variable instead of several call to the
    same getter => objective is to reduce the number of warning

--- pamela-core/src/main/java/org/openflexo/model/converter/ResourceLocationConverter.java
+++ pamela-core/src/main/java/org/openflexo/model/converter/ResourceLocationConverter.java
@@ -38,22 +38,18 @@
 
 package org.openflexo.model.converter;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.logging.Level;
 
 import org.openflexo.model.StringConverterLibrary.Converter;
 import org.openflexo.model.factory.ModelFactory;
 import org.openflexo.rm.Resource;
 import org.openflexo.rm.ResourceLocator;
-import org.openflexo.toolbox.FileUtils;
 
 public class ResourceLocationConverter extends Converter<Resource> {
 
-	private static final java.util.logging.Logger logger = org.openflexo.logging.FlexoLogger.getLogger(ResourceLocationConverter.class
-			.getPackage().getName());
+	private static final java.util.logging.Logger logger = org.openflexo.logging.FlexoLogger
+			.getLogger(ResourceLocationConverter.class.getPackage().getName());
 	private static final ResourceLocator rl = ResourceLocator.getResourceLocator();
-
 
 	public ResourceLocationConverter() {
 		super(Resource.class);
@@ -63,7 +59,7 @@ public class ResourceLocationConverter extends Converter<Resource> {
 	public Resource convertFromString(String value, ModelFactory factory) {
 		Resource resourceloc = ResourceLocator.locateResource(value);
 		if (resourceloc == null) {
-			logger.warning("Cannot find Resource: " + value );
+			logger.warning("Cannot find Resource: " + value);
 		}
 		if (logger.isLoggable(Level.FINE)) {
 			logger.fine("********* convertFromString " + value + " return " + resourceloc.toString());
