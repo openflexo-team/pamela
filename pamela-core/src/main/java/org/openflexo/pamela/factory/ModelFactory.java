5b8be8fe2b80298fd3eaf4db70cb9dbfdf0f23c2
Sylvain GUERIN <sylvain.guerin@openflexo.org>
2020-07-17 17:11:23 +0200

    Revert "MEDIUM / Fixed compile issue with Java11"
    This reverts commit ee3c2c970ecc8f9d17902ceca87b32b4dc10161b.

--- pamela-core/src/main/java/org/openflexo/pamela/factory/ModelFactory.java
+++ pamela-core/src/main/java/org/openflexo/pamela/factory/ModelFactory.java
@@ -438,7 +438,7 @@ public class ModelFactory implements IObjectGraphFactory {
 	public <I> Class<? extends I> getImplementingClassForInterface(Class<I> implementedInterface) throws ModelDefinitionException {
 		PAMELAProxyFactory<I> proxyFactory = getProxyFactory(implementedInterface, true);
 		if (proxyFactory != null) {
-			return (Class) proxyFactory.getSuperclass();
+			return proxyFactory.getSuperclass();
 		}
 		return null;
 	}
