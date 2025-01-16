4b0963f05d974d5ce5789463a4489b5f1b5532f1
Sylvain Guérin <sylvain.guerin@imt-atlantique.fr>
2025-01-16 11:28:36 +0100

    MEDIUM / Added ByteConverter

--- pamela-core/src/main/java/org/openflexo/pamela/model/StringConverterLibrary.java
+++ pamela-core/src/main/java/org/openflexo/pamela/model/StringConverterLibrary.java
@@ -76,6 +76,7 @@ public class StringConverterLibrary {
 		unmodifiableConverters = Collections.unmodifiableMap(converters);
 		addConverter(new BooleanConverter());
 		addConverter(new IntegerConverter());
+		addConverter(new ByteConverter());
 		addConverter(new ShortConverter());
 		addConverter(new LongConverter());
 		addConverter(new FloatConverter());
@@ -191,7 +192,8 @@ public class StringConverterLibrary {
 							Number returned = Double.parseDouble(value);
 							// System.out.println("Build a double: "+value);
 							return returned;
-						} catch (NumberFormatException e4) {}
+						} catch (NumberFormatException e4) {
+						}
 					}
 				}
 			}
@@ -247,6 +249,29 @@ public class StringConverterLibrary {
 
 		@Override
 		public String convertToString(Short value) {
+			return value.toString();
+		}
+
+	}
+
+	/**
+	 * Class defining how to convert Byte from/to String
+	 * 
+	 * @author sguerin
+	 */
+	private static class ByteConverter extends Converter<Byte> {
+
+		private ByteConverter() {
+			super(Byte.class);
+		}
+
+		@Override
+		public Byte convertFromString(String value, PamelaModelFactory factory) {
+			return Byte.valueOf(value);
+		}
+
+		@Override
+		public String convertToString(Byte value) {
 			return value.toString();
 		}
 
